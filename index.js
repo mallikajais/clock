7f987bdb4e9839823fb2bda534bf652e98919875
mallikajais <74813610+mallikajais@users.noreply.github.com>
2021-02-03 16:01:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,18 +10,18 @@ function analog(){
     document.getElementById("hr").style.transform=`rotate(`+hr+`deg)`;
     document.getElementById("min").style.transform=`rotate(`+min+`deg)`;
     document.getElementById("sec").style.transform=`rotate(`+sec+`deg)`;
-    if(h>=12 && h<=17){
-        document.getElementById("t").innerHTML=h+" : "+m+" : "+s+"  PM ";
-    }
-    if(h>=17 && h<=20){
-        document.getElementById("t").innerHTML=h+" : "+m+" : "+s+"  PM ";
-    }
-    if(h>=20 || h<=6){
-        document.getElementById("t").innerHTML=h+" : "+m+" : "+s+"  AM ";
-    }
-    else{
-        document.getElementById("t").innerHTML=h+" : "+m+" : "+s+"  AM ";
-    }
+//     if(h>=12 && h<=17){
+        document.getElementById("t").innerHTML=h+" : "+m+" : "+s+"   ";
+//     }
+//     if(h>=17 && h<=20){
+//         document.getElementById("t").innerHTML=h+" : "+m+" : "+s+"  PM ";
+//     }
+//     if(h>=20 || h<=6){
+//         document.getElementById("t").innerHTML=h+" : "+m+" : "+s+"  AM ";
+//     }
+//     else{
+//         document.getElementById("t").innerHTML=h+" : "+m+" : "+s+"  AM ";
+//     }
 
         
     
